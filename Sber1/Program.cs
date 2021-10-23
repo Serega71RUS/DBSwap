15f2db7a02df195ee0f995fa5be0ad60bb3ecfe9
Serega71RUS <mister.Cerz@yandex.ru>
2021-10-24 01:32:13 +0300

    List replaced with array

--- Sber1/Program.cs
+++ Sber1/Program.cs
@@ -12,48 +12,45 @@ namespace Sber1
         public static string Server(string json)
         {
             Auto CarList = JsonConvert.DeserializeObject<Auto>(json);
-            //foreach (var item in CarList)
-            //{
-                Console.WriteLine($"{CarList.Manufacturer}, {CarList.Model}, {CarList.HP}, {CarList.Date}");
-                Seller s;
-                switch (CarList.Manufacturer)
-                {
-                    case "Lada":
-                        s = new Seller { Salon = "Lada", Street = "Pr.Lenina", House = "25A", Price = 700000 };
-                        break;
-                    case "Nissan":
-                        s = new Seller { Salon = "Lada", Street = "Sovetskaya", House = "42", Price = 1552000 };
-                        break;
-                    case "Toyota":
-                        s = new Seller { Salon = "Lada", Street = "Oktabrskaya", House = "195", Price = 1245000 };
-                        break;
-                    default:
-                        s = new Seller { Salon = "NaN", Street = "NaN", House = "NaN", Price = 0 };
-                        break;
-                }
-                var newjson = JsonConvert.SerializeObject(s);
-                return newjson;
-            //}
-            //return null;
+            Console.WriteLine($"{CarList.Manufacturer}, {CarList.Model}, {CarList.HP}, {CarList.Date}");
+            Seller s;
+            switch (CarList.Manufacturer)
+            {
+                case "Lada":
+                    s = new Seller { Salon = "Lada", Street = "Pr.Lenina", House = "25A", Price = 700000 };
+                    break;
+                case "Nissan":
+                    s = new Seller { Salon = "Lada", Street = "Sovetskaya", House = "42", Price = 1552000 };
+                    break;
+                case "Toyota":
+                    s = new Seller { Salon = "Lada", Street = "Oktabrskaya", House = "195", Price = 1245000 };
+                    break;
+                default:
+                    s = new Seller { Salon = "NaN", Street = "NaN", House = "NaN", Price = 0 };
+                    break;
+            }
+            var newjson = JsonConvert.SerializeObject(s);
+            return newjson;
         }
         static void Main(string[] args)
         {
             using var db = new DbCont();
-            //var name = db.car.Select(c => c.Model).FirstOrDefault();
+            Auto[] CarList;
             var name = from auto in db.auto
                        select auto;
                        //select new { auto.Manufacturer, auto.Model, auto.HP, auto.Date };
             Console.WriteLine(name);
             var json = JsonConvert.SerializeObject(name);
             Console.WriteLine(json);
-            string path = @"text.json";
-            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
-            {
-                sw.WriteLine(json);
-            }
-            Console.WriteLine("Ok!");
-            List<Auto> CarList = JsonConvert.DeserializeObject<List<Auto>>(json);
-            foreach(var item in CarList)
+            //string path = @"text.json";
+            //using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            //{
+            //    sw.WriteLine(json);
+            //}
+            //Console.WriteLine("Ok!");
+            //List<Auto> CarList = JsonConvert.DeserializeObject<List<Auto>>(json);
+            CarList = JsonConvert.DeserializeObject<Auto[]>(json);
+            foreach (var item in CarList)
             {
                 Console.WriteLine($"{item.Manufacturer}, {item.Model}, {item.HP}, {item.Date}");
                 var newjson = JsonConvert.SerializeObject(item);
@@ -70,7 +67,6 @@ namespace Sber1
                 }).State);
                 db.SaveChanges();
             }
-
         }
     }
 }
